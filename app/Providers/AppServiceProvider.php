9967174a8c72d4ae7dd8a6e90f1752410414dd5f
bienvoakp <bienvenuakpo@gmail.com>
2024-07-15 17:10:14 +0200

    avance

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Carbon\Carbon;
+use Illuminate\Pagination\Paginator;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +24,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Carbon::setLocale('fr');
+        Paginator::useBootstrapFive();
+        Paginator::useBootstrapFour();
     }
 }
--- resources/views/_partials/link.blade.php
+++ resources/views/_partials/link.blade.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href=" {{ asset('vendor/fontawesome/css/all.min.css') }}">
+<link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
 
 
